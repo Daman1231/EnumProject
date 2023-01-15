97fc8a1d425c1b923bd53e093285f0b1e2a72b68
Daman1231 <daman.singh00786@gmail.com>
2023-01-14 22:18:49 -0500

    NaveenAutomationFramework

--- src/main/java/com/naveenautomation/Base/TestBase.java
+++ src/main/java/com/naveenautomation/Base/TestBase.java
@@ -1,11 +1,16 @@
 package com.naveenautomation.Base;
 
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.Platform;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 
 import com.naveenautomation.Browser.Browser;
@@ -16,16 +21,30 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class TestBase {
 
 	public static WebDriver driver;
-	public Browser DEFAULT_BROWSER = Browser.EDGE;
+	public Browser DEFAULT_BROWSER = Browser.GOOGLE_CHROME;
 	public static WebDriverEvents events;
 	public EventFiringWebDriver eventFiringWebDriver;
+	DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
 
 	public void launchBrowser() {
 
+		desiredCapabilities.setBrowserName("chrome");
+		desiredCapabilities.setPlatform(Platform.WINDOWS);
+		desiredCapabilities.setVersion("109.0.5414.74");
+
 		switch (DEFAULT_BROWSER) {
 		case GOOGLE_CHROME:
-			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
+			ChromeOptions options = new ChromeOptions();
+			options.merge(desiredCapabilities);
+			// options.setExperimentalOption("prefs", chromePreferences);
+//			WebDriverManager.chromedriver().setup();
+			try {
+				driver = new RemoteWebDriver(new URL("http://192.168.2.254:4444"), options);
+			} catch (MalformedURLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+//			driver = new ChromeDriver();
 			break;
 		case EDGE:
 			WebDriverManager.edgedriver().setup();
@@ -45,8 +64,7 @@ public class TestBase {
 		events = new WebDriverEvents();
 		eventFiringWebDriver.register(events);
 		driver = eventFiringWebDriver;
-		
-		
+
 		driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 		driver.manage().window().maximize();
 		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
